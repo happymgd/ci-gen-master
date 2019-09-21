7edc633ab15caacc36cd31979db39a525f128359
HaiYang <haiyang19890101@hotmail.com>
2019-09-22 01:30:06 +0800

    Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+CI-GEN
+======
+
+Codeigniter CRUD Code Generator With Bootstrap 3.1.1, Parsley 2.0 validation, easy to use & very simple
+
+
+Screenshoots:
+
+![ScreenShot](https://raw.github.com/brainlabs/ci-gen/master/ss.png)
+
+![ScreenShot](https://raw.github.com/brainlabs/ci-gen/master/ss2.png)
+
+Installation:
+=============
+
+1. Create a new database (if you don't have yet)
+2. Open URL: **http://localhost/ci-gen/install**
+3. Open URL: **http://localhost/ci-gen**
+4. Click menu "Builder" and customize the table's form
+5. Refresh web page
+6. Click menu "Tool", you will see the result
+
+
+Bahasa Indonesia:
+=================
+
+1. Rancang dan buat dulu database (kalau belum ada)
+2. Buka URL: **http://localhost/ci-gen/install**
+3. Buka URL: **http://localhost/ci-gen**
+4. Klik menu "Builder" dan sesuaikan form tabel
+5. Refresh halaman web
+6. Klik menu "Tool", anda akan lihat hasilnya
+
+
+## Donation
+
+If this project helps you reduce development time, please buy me some cups of coffee :)
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9AEJWZF6KRTFY)
+
+
---
+++ application/modules/builder/views/template/form.php
@@ -0,0 +1,54 @@
+
+<h2>{table_name}</h2>
+
+{php_open} 
+    echo $this->session->flashdata('notify');
+{php_close}
+
+
+<!-- <div class="row">
+    <div class="col-lg-12 col-md-12">        
+        {php_open} 
+                
+                echo create_breadcrumb();        
+                echo $this->session->flashdata('notify');
+                
+    {php_close}
+    </div>
+</div> -->
+
+{php_open} echo form_open(site_url('{table}/' . $action),'role="form" class="form-horizontal" id="form_{table}" parsley-validate'); {php_close}               
+<div class="panel panel-default">
+    <div class="panel-heading"><i class="glyphicon glyphicon-edit"></i> </div>
+     
+      <div class="panel-body">
+         
+            {forms}           
+               <div class="form-group">
+                   <label for="{field}" class="col-sm-2 control-label">{label}</label>
+                <div class="col-sm-6">                                   
+                  {php_open}                  
+                   echo {input}             
+                  {php_close}
+                 {php_open} echo form_error('{field}');{php_close}
+                </div>
+              </div> <!--/ {label_comment} -->
+               {/forms}
+           
+      </div> <!--/ Panel Body -->
+    <div class="panel-footer">   
+          <div class="row"> 
+              <!-- <div class="col-md-10 col-sm-12 col-md-offset-2 col-sm-offset-0"> -->
+              <div class="col-md-12 col-sm-12 col-lg-12 text-right">
+
+                   <a href="{php_open} echo site_url('{table}'); {php_close}" class="btn btn-default">
+                       <i class="glyphicon glyphicon-chevron-left"></i> Kembali
+                   </a> 
+                    <button type="submit" class="btn btn-primary" name="post">
+                        <i class="glyphicon glyphicon-floppy-save"></i> Simpan 
+                    </button>                  
+              </div>
+          </div>
+    </div><!--/ Panel Footer -->       
+</div><!--/ Panel -->
+{php_open} echo form_close(); {php_close}  
